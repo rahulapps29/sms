f59a8d2d7a46f45c6cf955d6ffdb7d037933c151
rahulapps29 <rahulapps29@gmail.com>
2024-11-16 02:07:12 +0530

    third commit

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.post("/send-sms", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4016;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
